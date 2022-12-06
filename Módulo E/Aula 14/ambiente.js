f2868ecb482a9e8352229da3c7db87075cbdc47a
Joao-Pedro-Leite <leite.jpedro@gmail.com>
2022-12-06 18:30:20 -0300

    .

---
+++ Módulo E/Aula 14/ambiente.js
@@ -0,0 +1,5 @@
+console.log ("Vai começar agora, acompanhe atentamente!")
+for (var c = 1; c <=10; c++){
+    console.log(`Precisa chegar até 10 e tá no ${c}`)
+}
+console.log("CHEGOU!!! Parabéns, você gnahou um total de 0 coisas")
