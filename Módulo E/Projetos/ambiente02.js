7b0687402093efcf5fe9e8f7ea01b99301ba8cfb
Joao-Pedro-Leite <leite.jpedro@gmail.com>
2022-12-09 18:17:19 -0300

    terminado os exercícios

--- Módulo E/Projetos/ambiente02.js
+++ Módulo E/Projetos/ambiente02.js
@@ -6,15 +6,29 @@ valor1 = Number(numero1.value)
 var numero2 = document.getElementById('numero2')
 valor2 = Number(numero2.value)
 
-if (numero1.value.length == 0 || numero2.value.length == 0) {
 
-    window.alert('Por favor insira um valor na caixa!')
+var select = document.getElementById('seltab')
+
+if (numero1.value.length == 0 || numero2.value.length == 0 || numero1.value < 0 || numero2.value < 0) {
+
+    window.alert('Por favor insira um valor válido na caixa!')
 } else {
-        res.innerHTML = `Resposta da tabuada... <br>`
         var v2 = 1
-        for (var tab = valor1; tab < valor1 * valor2; v2++,   tab = valor1 * v2 ) {
-            res.innerHTML += `${valor1} x ${v2} = ${tab} <br>`
+        select.innerHTML = ''
+        
+        for (var tab = valor1; tab <= valor1 * valor2; v2++,   tab = valor1 * v2 ) {
+
+
+
+
+            var item = document.createElement('option')
+            select.appendChild(item)
+
+            item.text += ` ${valor1} x ${v2} = ${tab} `
+
+            
         }
-            res.innerHTML += `${valor1} x ${v2} = ${tab}`
+
+            
     }
 }
